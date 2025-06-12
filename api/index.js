5e900655fe7fb93a2b6e6bc937d82ba2ca77a662
“Brijesh <brijeshseb11@gmail.com.com>
2025-06-12 14:58:34 +0530

    Add app.use(express.static(path.join(__dirname, '../public'))); to index.js

--- api/index.js
+++ api/index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/Uploads', express.static(path.join(__dirname, '..', 'Uploads')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
 const authRoutes = require('../routes/authRoutes');
