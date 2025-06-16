13ed94dc88014bf7d9461660aadd17897069d010
“Brijesh <brijeshseb11@gmail.com.com>
2025-06-16 14:52:29 +0530

    Edit mongoose error removing 2 items

--- api/index.js
+++ api/index.js
@@ -45,8 +45,6 @@ app.use('/api/project-logs', projectLogRoutes);
 async function startServer() {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000,
       bufferCommands: false,
     });
